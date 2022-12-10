9d11808fec5322f0a75c4bca5ba783cecbe4bbba
scott_li <developer.li1988@gmail.com>
2022-12-10 22:49:56 +0800

    商品功能 -- 查詢商品列表(基礎實作)

--- src/main/java/com/scott/springbootmall/dao/ProductDao.java
+++ src/main/java/com/scott/springbootmall/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.scott.springbootmall.dao;
 import com.scott.springbootmall.dto.ProductRequest;
 import com.scott.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
--- src/main/java/com/scott/springbootmall/service/ProductService.java
+++ src/main/java/com/scott/springbootmall/service/ProductService.java
@@ -3,7 +3,11 @@ package com.scott.springbootmall.service;
 import com.scott.springbootmall.dto.ProductRequest;
 import com.scott.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
