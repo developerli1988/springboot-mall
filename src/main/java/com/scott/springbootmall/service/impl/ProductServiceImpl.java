09dcff25f8d1133eb1500d434e796489b46d8058
scott_li <developer.li1988@gmail.com>
2022-12-11 19:29:21 +0800

    商品功能 -- 改善查詢參數傳遞

--- src/main/java/com/scott/springbootmall/service/impl/ProductServiceImpl.java
+++ src/main/java/com/scott/springbootmall/service/impl/ProductServiceImpl.java
@@ -1,7 +1,7 @@
 package com.scott.springbootmall.service.impl;
 
-import com.scott.springbootmall.constant.ProductCategory;
 import com.scott.springbootmall.dao.ProductDao;
+import com.scott.springbootmall.dto.ProductQueryParams;
 import com.scott.springbootmall.dto.ProductRequest;
 import com.scott.springbootmall.model.Product;
 import com.scott.springbootmall.service.ProductService;
@@ -17,8 +17,8 @@ public class ProductServiceImpl implements ProductService {
     private ProductDao productDao;
 
     @Override
-    public List<Product> getProducts(ProductCategory category, String search) {
-        return productDao.getProducts(category, search);
+    public List<Product> getProducts(ProductQueryParams productQueryParams) {
+        return productDao.getProducts(productQueryParams);
     }
 
     @Override
