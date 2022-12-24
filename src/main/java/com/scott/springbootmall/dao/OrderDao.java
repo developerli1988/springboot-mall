94a1fa67130e469bb02f6d41eefb6d1aafcffb21
scott_li <developer.li1988@gmail.com>
2022-12-24 19:36:29 +0800

    訂單功能 -- 創建訂單(上)

---
+++ src/main/java/com/scott/springbootmall/dao/OrderDao.java
@@ -0,0 +1,12 @@
+package com.scott.springbootmall.dao;
+
+import com.scott.springbootmall.model.OrderItem;
+
+import java.util.List;
+
+public interface OrderDao {
+
+    Integer createOrder(Integer userId, Integer totalAmount);
+
+    void createOrderItems(Integer orderId, List<OrderItem> orderItemList);
+}
---
+++ src/main/java/com/scott/springbootmall/service/impl/OrderServiceImpl.java
@@ -0,0 +1,52 @@
+package com.scott.springbootmall.service.impl;
+
+import com.scott.springbootmall.dao.OrderDao;
+import com.scott.springbootmall.dao.ProductDao;
+import com.scott.springbootmall.dto.BuyItem;
+import com.scott.springbootmall.dto.CreateOrderRequest;
+import com.scott.springbootmall.model.OrderItem;
+import com.scott.springbootmall.model.Product;
+import com.scott.springbootmall.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class OrderServiceImpl implements OrderService {
+
+    @Autowired
+    private OrderDao orderDao;
+
+    @Autowired
+    private ProductDao productDao;
+
+    @Transactional
+    @Override
+    public Integer createOrder(Integer userId, CreateOrderRequest createOrderRequest) {
+        int totalAmount = 0;
+        List<OrderItem> orderItemList = new ArrayList<>();
+
+        for(BuyItem buyItem : createOrderRequest.getBuyItemList()){
+            Product product = productDao.getProductById(buyItem.getProductId());
+
+            int amount = buyItem.getQuantity() * product.getPrice();
+            totalAmount = totalAmount + amount;
+
+            OrderItem orderItem = new OrderItem();
+            orderItem.setProductId(buyItem.getProductId());
+            orderItem.setQuantity(buyItem.getQuantity());
+            orderItem.setAmount(amount);
+
+            orderItemList.add(orderItem);
+        }
+
+        Integer orderId = orderDao.createOrder(userId, totalAmount);
+
+        orderDao.createOrderItems(orderId, orderItemList);
+
+        return orderId;
+    }
+}
